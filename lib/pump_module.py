0069f67362d17c9299f83b3f085142cde49e6b0b
Dave Peacock <dave.peacock@ebbcarbon.com>
2023-07-25 09:12:01 -0700

    Refactor, add bytes() call to ensure encoding this was some WIP locally, note that we need to add explicit 'R' at end of string to do anything

--- lib/pump_module.py
+++ lib/pump_module.py
@@ -1,4 +1,5 @@
 import serial
+import time
 
 PUMP_PORT = '/dev/ttyUSB1'
 BAUD_RATE = 9600
@@ -14,12 +15,22 @@ def write_cmd(port, base_cmd):
 
     # All cmds are preceeded by '/', then the address, which we always assume to be 1 here
     # all cmds are separated by a carriage return '\r'
-    cmd = f'/1{cmd}\r'
-    length = port.write(f'{cmd}\r')
+    cmd = f'/1{base_cmd}\r'
+    length = port.write(bytes(cmd, 'utf-8'))
+
+    # Docs indicate we can move too fast -- 8 commands a second is about all the device chan handle
+    # manual sleep after each command, and then a read
+    time.sleep(0.150)
     # verify that we actually wrote cmd
     assert length == len(cmd)
+    read_response(port)
 
-    serial_string = port.readline()
+
+def read_response(port):
+    """
+    Read a response from the port
+    """
+    response = port.readline()
     print(f'Read pump response: |{response}|')
 
 
@@ -27,11 +38,29 @@ def pump_test():
     serial_port =  serial.Serial(PUMP_PORT, baudrate=BAUD_RATE, timeout=SERIAL_TIMEOUT)
 
     write_cmd(serial_port, '?')
-    write_cmd(serial_port, '?1')
-    write_cmd(serial_port, '?2')
-    write_cmd(serial_port, '?3')
+    # We might want to include this in the write_cmd -- docs indicate we should send this empty
+    # string and look for the 'ready' ("`") response
+    write_cmd(serial_port, '')
+    #write_cmd(serial_port, '?31')
+    #write_cmd(serial_port, '')
+    #write_cmd(serial_port, '~V')
+    #write_cmd(serial_port, '')
+    #write_cmd(serial_port, '?8')
+    #write_cmd(serial_port, '')
+    #write_cmd(serial_port, '?2')
+    #write_cmd(serial_port, '?3')
 
-    # Test other commands here
+    # Docs indicate W4 should be the first command we send, to initialize
+    write_cmd(serial_port, 'W4')
+    time.sleep(1)
+    write_cmd(serial_port, '')
+    write_cmd(serial_port, 'A24000')
+    write_cmd(serial_port, '')
+    time.sleep(5)
+
+    #write_cmd(serial_port, 'o3')
+    #time.sleep(3)
+
 
 
 if __name__ == '__main__' :
