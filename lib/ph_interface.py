79058a5ee41fb31b3e77d2a6a19b3235d5f19954
Dave Peacock <dave.peacock@ebbcarbon.com>
2023-07-25 13:29:33 -0700

    wip, cleanup

--- lib/ph_interface.py
+++ lib/ph_interface.py
@@ -18,23 +18,3 @@ class pHInterface:
     def read_emf_pH(self):
         """ """
         raise NotImplementedError("Use derived pH implementation class!")
-
-
-def readpH(s):
-
-    s.reset_input_buffer()
-
-    serialString = ""
-    while 1:
-        if s.in_waiting > 0:
-            serialString = s.readline()
-            try:
-                serialString = serialString.decode("Ascii")
-                arr = serialString.split(",")
-                if serialString.find("\rA211 pH") != -1:
-                    i = arr.index(" mV")
-                    emf = arr[i - 1]
-                    pH = arr[i - 3]
-                    return emf, pH
-            except:
-                pass
