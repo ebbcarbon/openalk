d9f4bd1fc9ae56ad248ee5190bac4629fe095346
Anthony Canalungo <acanalungo@gmail.com>
2023-09-28 17:10:49 -0700

    mapping out pump and ph interfaces more comprehensively

--- lib/services/pump/pump_testing.py
+++ lib/services/pump/pump_testing.py
@@ -1,7 +1,8 @@
 import serial
 import time
 
-PUMP_PORT = "/dev/ttyUSB1"
+
+PUMP_PORT_ADDR = "/dev/ttyUSB0"
 BAUD_RATE = 9600
 SERIAL_TIMEOUT = 2
 
@@ -32,6 +33,8 @@ def read_response(port):
     Read a response from the port
     """
     response = port.readline()
+    print(response)
+    print(response.decode("utf-8"))
     print(f"Read pump response: |{response}|")
 
 
@@ -62,6 +65,96 @@ def pump_test():
     # write_cmd(serial_port, 'o3')
     # time.sleep(3)
 
+def initialize_pump():
+    pass
+
+def move_to_absolute_position(port, pos):
+    pass
+
+def fill_syringe():
+    '''From where?'''
+    pass
+
+def empty_syringe():
+    '''To where?'''
+    pass
+
+def wash():
+    pass
+
+def dispense(steps):
+    pass
+
+def aspirate(steps):
+    pass
+
+def set_valve_to_input():
+    pass
+
+def set_valve_to_output():
+    pass
+
+def check_syringe_position():
+    pass
+
+def check_valve_position():
+    pass
+
+def check_response(res):
+    # Split apart decoded response string
+    pass
+
+
+'''
+***Pump speaks ASCII only***
+
+Valid communications response always begins with /0 (host address)
+Any individual pump module can be queried/commanded with its number, e.g. /1
+After /0, the backtick (`) indicates "not busy", @ means "busy", and any other
+letter means the module is reporting an error (this is the status byte)
+After the status byte, there can also be a return value in response to a query
+
+Commands (all require an "R" after the command to run)
+"W4" will initialize the syringe to the start position, must be run at startup
+
+"An" will command an absolute move to n steps from the valve
+"Dn" will dispense n steps toward the valve
+"Pn" will aspirate n steps away from the valve
+
+"I" switches valve to input position (port A -> syringe)
+"O" switches valve to output position (syringe -> port B)
+"B" switches valve to bypass position (port A -> port B)
+
+Queries (do not require an "R")
+"?" queries syringe position, given in absolute steps from the valve
+"?8" queries valve status, with 1 == input, 2 == bypass, 3 == output
+
+
+'''
+
+def testing():
+    port = serial.Serial(port=PUMP_PORT_ADDR, baudrate=BAUD_RATE, timeout=SERIAL_TIMEOUT)
+    print(port)
+    # cmd = "IR"
+    cmd = "D24000R"
+    # cmd = "?8"
+    # cmd = "W4R"
+    print(cmd)
+    cmd_string = f"/1{cmd}\r"
+    print(cmd_string)
+    cmd_encoded = cmd_string.encode("ascii")
+    print(cmd_encoded)
+    port.write(cmd_encoded)
+    res_encoded = port.readline().split()[0]
+    res = res_encoded.decode("ascii").rstrip()
+
+    host_status = res[:2]
+    module_status = res[2]
+    msg = res[3:]
+
+    print(res)
+    print(msg)
+
 
 if __name__ == "__main__":
-    main()
+    testing()
