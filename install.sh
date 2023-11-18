92cb37d12b798a0d57ee978bb7f38f3766fb1871
Anthony Canalungo <acanalungo@gmail.com>
2023-11-17 17:18:13 -0800

    change permissions on install

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ echo "Installing with repo location $GIT_TOPLEVEL"
 
 sed "s+<REPO_LOCATION>+$GIT_TOPLEVEL+g" install/ta.desktop > $HOME/Desktop/ta.desktop
 
+chmod a+x $HOME/Desktop/ta.desktop
+
 echo "Installed."
