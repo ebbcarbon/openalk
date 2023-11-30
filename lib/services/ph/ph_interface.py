7b3415f60b2afb4e2cf7d8edee4ca1f1c5371ce5
Anthony Canalungo <acanalungo@gmail.com>
2023-11-30 15:58:03 -0800

    testing for ph modules

--- lib/services/ph/ph_interface.py
+++ lib/services/ph/ph_interface.py
@@ -1,10 +1,10 @@
 
 class pHInterface:
     """
-    This class defines an interface to a pH probe.
+    This class defines the base interface to a pH probe.
     """
-    def __init__(self):
+    def __init__(self) -> None:
         self.SERIAL_PORT_LOC = None
 
-    def get_measurement(self):
+    def get_measurement(self) -> dict:
         raise NotImplementedError("Use derived pH implementation class!")
--- lib/services/ph/tests/ph_modules_test.py
+++ lib/services/ph/tests/ph_modules_test.py
@@ -1,21 +1,34 @@
 # Write unit tests for ph_modules logic here
 # Tests MUST start with `test_` for pytest to find them
 
+import serial
+import pytest
 from unittest.mock import patch
 
 from lib.services.ph import orion_star
 
-# @patch.object(pH_meter_simulated, "read_emf_pH")
-# def test_ph_simulated_module(mock_read_emf_ph):
-#     """
-#     Dummy test demonstrating mocking.
-#     TODO: Replace this with actual unit test logic.
-#     """
-#     mock_read_emf_ph.return_value = "banana"
-#
-#     result = pH_meter_simulated().read_emf_pH()
-#     assert result == "banana"
+def test_init_fails_without_serial() -> None:
+    with pytest.raises(serial.SerialException):
+        ph_meter = orion_star.OrionStarA215()
 
-# def test_dummy():
-#     ph_meter = orion_star.OrionStarA215()
-#     assert True is True
+@patch.object(orion_star.OrionStarA215, '__init__')
+def test_build_serial_command_A215(mock_init) -> None:
+    mock_init.return_value = None
+    ph_meter = orion_star.OrionStarA215()
+
+    cmd = "GETMEAS"
+    encoded_cmd = ph_meter.build_serial_command(cmd)
+    assert isinstance(encoded_cmd, bytes)
+    assert encoded_cmd == b"GETMEAS\r"
+
+@patch.object(orion_star.OrionStarA215, '__init__')
+def test_check_response_A215(mock_init) -> None:
+    mock_init.return_value = None
+    ph_meter = orion_star.OrionStarA215()
+
+    dummy_res = "\n \n \nchannelname---SAMPLE,CH-1,pH,7.0,pH,0.0,mV,25.0,C"
+    res_encoded = dummy_res.encode("ascii")
+
+    res = ph_meter.check_response(res_encoded)
+    assert isinstance(res, dict)
+    assert res == {"pH": "7.0", "mV": "0.0", "temp": "25.0"}
