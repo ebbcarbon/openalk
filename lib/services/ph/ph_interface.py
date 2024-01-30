28bd8458253cbf8093cc32706141250e9106f440
Anthony Canalungo <acanalungo@gmail.com>
2024-01-29 16:59:30 -0800

    PEP8 formatting and Google style docstrings for ph interface

--- lib/services/ph/ph_interface.py
+++ lib/services/ph/ph_interface.py
@@ -1,10 +1,12 @@
 
 class pHInterface:
     """
-    This class defines the base interface to a pH probe.
+    This class defines the high-level operations of the pH meter;
+    specific implementation classes should be used for each device, with
+    this class as the parent.
     """
     def __init__(self) -> None:
-        self.SERIAL_PORT_LOC = None
+        self.serial_port_loc = None
 
     def get_measurement(self) -> dict:
-        raise NotImplementedError("Use derived pH implementation class!")
+        raise NotImplementedError("Use derived pH meter implementation class!")
