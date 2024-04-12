8494755a8f3cfd72e0416a7c66c813579859f001
Anthony Canalungo <acanalungo@gmail.com>
2024-04-11 19:45:00 -0700

    starting fixing pump tests

--- lib/services/pump/tests/pump_modules_test.py
+++ lib/services/pump/tests/pump_modules_test.py
@@ -1,16 +1,32 @@
-import serial
+# Write unit tests for pump modules here
+# Tests MUST start with `test_` for pytest to find them
+
 import pytest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from lib.services.pump import norgren
 
-def test_init_fails_without_serial() -> None:
-    with pytest.raises(serial.SerialException):
-        pump = norgren.VersaPumpV6()
+SERIAL_REPR = "Serial<id=0xa81c10, open=True>(port='/dev/ttyUSB0', \
+    baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=2, xonxoff=0,\
+    rtscts=0)"
 
-@patch.object(norgren.VersaPumpV6, '__init__')
-def test_build_serial_command_norgren(mock_init) -> None:
-    mock_init.return_value = None
+@patch('serial.Serial', Mock(return_value=SERIAL_REPR))
+def test_open_serial_port_success_norgren() -> None:
+    pump = norgren.VersaPumpV6()
+    result = pump.open_serial_port()
+    assert result == True
+
+def test_open_serial_port_failure_norgren() -> None:
+    pump = norgren.VersaPumpV6()
+    result = pump.open_serial_port()
+    assert result == False
+
+def test_change_serial_port_from_default_norgren() -> None:
+    pump = norgren.VersaPumpV6()
+    pump.open_serial_port(port='/dev/ttyUSB1')
+    assert pump.serial_port_loc == '/dev/ttyUSB1'
+
+def test_build_serial_command_norgren() -> None:
     pump = norgren.VersaPumpV6()
 
     cmd = "W4R"
@@ -18,36 +34,26 @@ def test_build_serial_command_norgren(mock_init) -> None:
     assert isinstance(encoded_cmd, bytes)
     assert encoded_cmd == b"/1W4R\r"
 
-@patch.object(norgren.VersaPumpV6, "get_syringe_position")
-@patch.object(norgren.VersaPumpV6, "__init__")
-def test_check_volume_available_norgren(mock_init,
-                                      mock_get_syringe_position) -> None:
-    mock_init.return_value = None
+def test_check_volume_available_norgren() -> None:
     pump = norgren.VersaPumpV6()
-    pump.liters_per_step = 0.00000005
 
-    mock_get_syringe_position.return_value = 0
+    pump.get_syringe_position = Mock(return_value=0)
     volume_available = pump.check_volume_available(volume=0.0025)
     assert volume_available == False
 
-    mock_get_syringe_position.return_value = 48000
+    pump.get_syringe_position = Mock(return_value=48000)
     volume_available = pump.check_volume_available(volume=0.0005)
     assert volume_available == True
 
-@patch.object(norgren.VersaPumpV6, '__init__')
-def test_liters_to_steps_norgren(mock_init) -> None:
-    mock_init.return_value = None
+def test_liters_to_steps_norgren() -> None:
     pump = norgren.VersaPumpV6()
-    pump.liters_per_step = 0.00000005
 
     dummy_vol = 0.0025
     steps = pump.liters_to_steps(volume=dummy_vol)
     assert isinstance(steps, int)
     assert steps == (int(dummy_vol / pump.liters_per_step))
 
-@patch.object(norgren.VersaPumpV6, '__init__')
-def test_check_response_norgren(mock_init) -> None:
-    mock_init.return_value = None
+def test_check_response_norgren() -> None:
     pump = norgren.VersaPumpV6()
 
     dummy_res = "/0`48000"
